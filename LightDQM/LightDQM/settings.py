bc9ff77562ea458c800424bd76d8d30eff7f585b
Mykhailo Dalchenko <mykhailo.dalchenko@cern.ch>
2015-12-18 12:36:02 +0100

    Adjusted to run @gem904daq01

--- LightDQM/LightDQM/settings.py
+++ LightDQM/LightDQM/settings.py
@@ -56,9 +56,10 @@ ROOT_URLCONF = 'LightDQM.urls'
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'LightDQM/static'),
-    '/home/mdalchen/work/django/LightDQM/LightDQM/',
-    '/Users/mexanick/work/ldqm-browser/LightDQM/',
-    '/Users/mexanick/work/ldqm-browser/LightDQM/LightDQM/',
+    ('data_dqm','/data/bigdisk/GEM-Data-Taking/GEM-LightDQM/'),
+#    '/home/mdalchen/work/django/LightDQM/LightDQM/',
+#    '/Users/mexanick/work/ldqm-browser/LightDQM/',
+#    '/Users/mexanick/work/ldqm-browser/LightDQM/LightDQM/',
     ]
 
 TEMPLATES = [
