9ec3fae4c760ba146bf31eebd42add6051775ad0
Mykhailo Dalchenko <mykhailo.dalchenko@cern.ch>
2016-02-09 14:10:00 -0600

    Adding state tables in DB

--- LightDQM/ldqm_db/models.py
+++ LightDQM/ldqm_db/models.py
@@ -1,14 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class HWstate(models.Model):
+  HWID = models.CharField(max_length=30)
+  State = models.CharField(max_length=1, default='0')
+
+class SystemState(models.Model):
+  amcStates = models.ManyToManyField(HWstate, related_name='amc_states')
+  gebStates = models.ManyToManyField(HWstate, related_name='geb_states')
+  vfatStates = models.ManyToManyField(HWstate, related_name='vfat_states')
+
 class VFAT(models.Model):
   ChipID = models.CharField(max_length=6, default='0xdead')
-  Status = models.CharField(max_length=1, default='0')
+  def __unicode__(self):
+    return self.ChipID
 
 class GEB(models.Model):
   Type = models.CharField(max_length=30)
   ChamberID = models.CharField(max_length=30)
-  Status = models.CharField(max_length=1, default='0')
   vfats = models.ManyToManyField(VFAT)
 
   def __unicode__(self):
@@ -17,7 +26,6 @@ class GEB(models.Model):
 class AMC(models.Model):
   BoardID = models.CharField(max_length=30)
   Type = models.CharField(max_length=30)
-  Status = models.CharField(max_length=1, default='0')
   gebs = models.ManyToManyField(GEB)
   def __unicode__(self):
     return self.Type # or better id?
@@ -31,7 +39,7 @@ class Run(models.Model):
   Station = models.CharField(max_length=10)
   amcs = models.ManyToManyField(AMC)
   Status = models.BooleanField(default=False) #indicates if the dqm had processed the run
-
+  State = models.ForeignKey(SystemState, null=True, blank=True, default=None)
   def __unicode__(self):
     return self.Name
   
