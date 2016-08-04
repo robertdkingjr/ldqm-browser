d7427edd8b861a5435082350ff591eca03cb8c6c
Robert King <robertdkingjr@gmail.com>
2016-08-04 21:32:53 +0200

    Daemon occasionally does full offline print

--- LightDQM/LightDQM/dqm_daemon.py
+++ LightDQM/LightDQM/dqm_daemon.py
@@ -5,7 +5,7 @@ from ldqm_db.models import Run
 import time
 import glob
 
-def process_chunk(m_filename, chunk):
+def process_chunk(m_filename, chunk, offline_dqm = False):
   global is_first
   print "Process chunk with base filename %s\n" %(m_filename)
   t_filename = m_filename+"_chunk_"+str(chunk)
@@ -13,11 +13,6 @@ def process_chunk(m_filename, chunk):
   call_command =  os.getenv('BUILD_HOME')+'/gem-light-dqm/gemtreewriter/bin/'+os.getenv('XDAQ_OS')+'/'+os.getenv('XDAQ_PLATFORM')+'/unpacker'
   command_args = "/tmp/"+t_filename+".dat sdram"
   call([call_command+' '+command_args],shell=True)
-#create dirs in tmp
-  for i in range (24):
-    call(["mkdir -p /tmp/dqm_hists/%s"%(i)],shell=True)
-  call(["mkdir -p /tmp/dqm_hists/OtherData"],shell=True)
-  call(["mkdir -p /tmp/dqm_hists/canvases"],shell=True)
 #call dqm
   call_command =  os.getenv('BUILD_HOME')+'/gem-light-dqm/dqm-root/bin/'+os.getenv('XDAQ_OS')+'/'+os.getenv('XDAQ_PLATFORM')+'/dqm'
   command_args = "/tmp/"+t_filename+".raw.root"
@@ -42,7 +37,10 @@ def process_chunk(m_filename, chunk):
     call(["mv "+ "/tmp/" + t_filename+".dat" + " " + "/tmp/" + m_filename+".dat"],shell=True)
 
 #call dqm printer
-  call_command =  os.getenv('BUILD_HOME')+'/gem-light-dqm/dqm-root/bin/'+os.getenv('XDAQ_OS')+'/'+os.getenv('XDAQ_PLATFORM')+'/onlineprinter'
+  if offline_dqm:
+    call_command = os.getenv('BUILD_HOME')+'/gem-light-dqm/dqm-root/bin/'+os.getenv('XDAQ_OS')+'/'+os.getenv('XDAQ_PLATFORM')+'/gtprinter'
+  else:
+    call_command =  os.getenv('BUILD_HOME')+'/gem-light-dqm/dqm-root/bin/'+os.getenv('XDAQ_OS')+'/'+os.getenv('XDAQ_PLATFORM')+'/onlineprinter'
   command_args = "/tmp/"+m_filename+".analyzed.root"
   os.system(call_command+' '+command_args)
 
@@ -71,13 +69,18 @@ def run_dqm():
     fname_base = run.Name
     globname = glob.glob('/tmp/'+fname_base+'_chunk_*.dat')
     print len(globname), 'chunks remaining to process for',run.Name
+    glob_count=0
     for fname in globname:
+      glob_count+=1
       print fname
       chunk = int(fname[fname.find('chunk')+6:fname.find('.dat')])
       print 'Chunk:',chunk
       file_exist = os.path.isfile(fname) 
       if file_exist:
-        process_chunk(fname_base, chunk)
+        if glob_count==len(globname):
+          process_chunk(fname_base, chunk, True) #do full print on last glob
+        else:
+          process_chunk(fname_base, chunk, False)
 
 
 
